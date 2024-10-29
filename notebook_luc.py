d273a40f27f1ec2f5267a30d44447c076d38e70a
lucbrd8 <lucbiard57@gmail.com>
2024-10-29 17:19:25 +0100

    Commentaires données dataset

--- notebook_luc.py
+++ notebook_luc.py
@@ -1,7 +1,9 @@
+#Importations
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Ouverture du fichier avec les bons noms de colonne
 df = pd.read_csv('donnes_luc.csv',sep=',',header=96)
 file=open('donnes_luc.csv','r')
 chaine_cols=file.readlines()[:96]
@@ -22,4 +24,16 @@ for ligne in chaine_cols[3:-1] :
 correspondance=[ligne.split(":") for ligne in ligne_propre]
 for nom_colonne in correspondance:
     df.rename(columns={nom_colonne[0] : nom_colonne[1]}, inplace=True)
-df
+
+len(df.columns)
+#Il y a 92 informations pour chacune des 35 000 planètes
+df.shape[0]*df.shape[1]-df.isna().sum().sum()
+
+# +
+# Il y a 2 318 924 valeurs : très gros dataset
+# -
+
+df.isna().sum().sort_values()[-15:]
+
+# +
+#Beaucoup de valeurs manquent dans les catégories les plus poussées.
