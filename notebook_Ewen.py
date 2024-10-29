7010902326174a34e01fa37169c6d557d3fcf43f
Ewen Marzin <ewenmarzin@icloud.com>
2024-10-29 16:36:51 +0100

    plusieurs tris ou sélections de données

--- notebook_Ewen.py
+++ notebook_Ewen.py
@@ -14,3 +14,41 @@
 # ---
 
 # %%
+import pandas as pd
+import numpy as np
+df = pd.read_csv('donnees.csv', header= 96)
+df
+
+# %%
+mask = df['disc_year'] < 2000
+df[mask]
+
+# %%
+mask = (df['discoverymethod'] == 'Radial Velocity') & (df['disc_year'] > 2015)
+mask.value_counts()
+
+# %%
+by_station = df.groupby(by= 'disc_facility')
+len(by_station)
+
+# %%
+by_station.size()
+
+# %%
+max(by_station.size())
+
+# %%
+mask = (df['disc_facility'] == 'Multiple Observatories')
+mask.value_counts()
+
+# %%
+mask = (df['disc_facility'] == 'Okayama Astrophysical Observatory')
+mask.value_counts()
+
+# %%
+df.pivot_table(values = 'disc_year', index = 'disc_facility', columns = 'sy_pnum', aggfunc = max)
+
+# %%
+df.pivot_table(values = 'disc_year', index = 'disc_facility', columns = 'sy_pnum', aggfunc = min)
+
+# %%
